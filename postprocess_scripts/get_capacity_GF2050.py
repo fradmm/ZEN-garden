3421d7957f16a7ad0c97f78cfb5555fe223aa7d3
fradmm <fdemarco@ethz.ch>
2024-05-06 16:22:33 +0200

    Update get_capacity_GF2050.py

--- postprocess_scripts/get_capacity_GF2050.py
+++ postprocess_scripts/get_capacity_GF2050.py
@@ -8,20 +8,21 @@ from zen_garden.postprocess.results.results import Results
 
 def main():
     #zen_garden
+
     out_folder = "outputs/GF2050"
     r = Results(out_folder)
     capacity = r.get_total("capacity").reset_index()
     capacity['year_construction'] = 2049.0
 
     for tech in r.get_system().set_conversion_technologies:
-        filepath = "GF2050_fix_df_forced/set_technologies/set_conversion_technologies/" + tech + "/"
+        filepath = "GF2050_1/set_technologies/set_conversion_technologies/" + tech + "/"
 
         capacity_existing = capacity[capacity.technology == tech].loc[:,['location','year_construction',0]].copy()
         capacity_existing = capacity_existing.rename(columns={'location': 'node','year_construction':'year_construction', 0: 'capacity_existing'})
         capacity_existing.to_csv(filepath + "capacity_existing.csv", index=False)
 
     for tech in r.get_system().set_storage_technologies:
-        filepath = "GF2050_fix_df_forced/set_technologies/set_storage_technologies/" + tech + "/"
+        filepath = "GF2050_1/set_technologies/set_storage_technologies/" + tech + "/"
 
         capacity_existing = capacity[(capacity.technology == tech) & (capacity.capacity_type == 'power')].loc[:, ['location', 'year_construction', 0]].copy()
         capacity_existing = capacity_existing.rename(columns={'location': 'node', 'year_construction': 'year_construction', 0: 'capacity_existing'})
@@ -32,11 +33,13 @@ def main():
         capacity_existing_energy.to_csv(filepath + "capacity_existing_energy.csv", index=False)
 
     for tech in r.get_system().set_transport_technologies:
-        filepath = "GF2050_fix_df_forced/set_technologies/set_transport_technologies/" + tech + "/"
+        filepath = "GF2050_1/set_technologies/set_transport_technologies/" + tech + "/"
 
         capacity_existing = capacity[capacity.technology == tech].loc[:,['location','year_construction',0]].copy()
         capacity_existing = capacity_existing.rename(columns={'location': 'edge','year_construction':'year_construction', 0: 'capacity_existing'})
         capacity_existing.to_csv(filepath + "capacity_existing.csv", index=False)
+
+
 
     a=1
 
