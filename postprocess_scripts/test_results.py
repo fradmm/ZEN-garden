74698dfc29721854d1a596cfac35b54f263c8088
fradmm <fdemarco@ethz.ch>
2024-09-12 13:18:25 +0200

    my script results

--- postprocess_scripts/test_results.py
+++ postprocess_scripts/test_results.py
@@ -8,11 +8,17 @@ from zen_garden.postprocess.results.results import Results
 
 def main():
     #zen_garden
-    out_folder = "outputs/1_base_case"
+    out_folder = "outputs/7_multiple_time_steps_per_year_1_inflow"
 
-    r = Results(out_folder)
+    r = Results("outputs/20240603_GF")
 
+    r_0 = Results("outputs/7_multiple_time_steps_per_year_inflow")
+    r_1 = Results("outputs/7_multiple_time_steps_per_year_1_inflow")
+    r_N = Results("outputs/7_multiple_time_steps_per_year_inflow_no_spillage")
 
+    r_0_PV = Results("outputs/7_multiple_time_steps_per_year_inflow_PV_opex")
+    r_1_PV = Results("outputs/7_multiple_time_steps_per_year_1_inflow_PV_opex")
+    r_10_PV = Results("outputs/7_multiple_time_steps_per_year_10_inflow_PV_opex")
 
     a=1
 
