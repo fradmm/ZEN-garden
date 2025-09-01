1e95c1efbc090daaa55a2fddee1412decd2eb7f5
fradmm <fdemarco@ethz.ch>
2025-09-01 12:48:24 +0200

    Bug fixed for year specific tsa, sequence time steps

--- zen_garden/preprocess/time_series_aggregation.py
+++ zen_garden/preprocess/time_series_aggregation.py
@@ -269,8 +269,8 @@ class TimeSeriesAggregation(object):
                 for element,ts in elements_time_series: #zip(elements,time_series):
                     unstacked = year_raw_ts.unstack(header_set_time_steps)
                     if (element,ts) in self.optimization_setup.year_specific_ts[year].keys():
-                        ii = unstacked[element, ts].index
-                        unstacked[element,ts] = self.optimization_setup.year_specific_ts[year][(element,ts)][ii]
+                        ii = unstacked[element, ts].index #TODO to avoid indexing past lexsort depth PerformanceWarning
+                        unstacked[element,ts] = self.optimization_setup.year_specific_ts[year][(element,ts)][ii]  #TODO to avoid indexing past lexsort depth PerformanceWarning
                     else:
                         ts_adjusted = self.multiply_yearly_variation(self.optimization_setup.get_element(Element,element), ts, year_raw_ts.unstack(header_set_time_steps)[element,ts], year)
                         unstacked[element,ts] = ts_adjusted
@@ -286,7 +286,7 @@ class TimeSeriesAggregation(object):
                     self.single_ts_tsa()
                 # overwrite time_step_sequence here
                 base_time_steps = self.energy_system.time_steps.decode_time_step(year, "yearly")
-                new_sequence_time_steps[base_time_steps][:,0] = self.sequence_time_steps
+                new_sequence_time_steps[base_time_steps.squeeze()] = self.sequence_time_steps
                 # save year specific TSA results
                 self.year_specific_tsa[year] = self.typical_periods
         return new_sequence_time_steps
