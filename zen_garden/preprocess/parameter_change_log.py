f83ddfec08c9c263bae313f21db4c55728af850d
fradmm <153306382+fradmm@users.noreply.github.com>
2026-04-23 17:32:22 +0200

    min_full_load_hours_fraction

--- zen_garden/preprocess/parameter_change_log.py
+++ zen_garden/preprocess/parameter_change_log.py
@@ -1,6 +1,9 @@
 def parameter_change_log():
     log_dict = {
-
+        "min_full_load_hours_fraction": {
+            "default_value": 0,  # only 0, 1, or 'inf' are allowed
+            "unit": "min_load"
+        }
       #  "updated_name": "outdated_name",
 
     #    "new_parameter_name": {
